d9cc16a90d4f68fee77b31c3b7ed247587eecdad
Giancarlo Malugani <monkodev@gmail.com>
2017-05-12 14:22:25 -0300

    Cambia y agrega logo

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,6 @@
 class QuestionsController < ApplicationController
+	before_action :authenticate_user!, except: [:index, :show]
 	def index
-		
+		@question=Question.order(created_at: :desc)
 	end
 end
