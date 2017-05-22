70529039b42132ee4db8fec8bc0408502004848d
Giancarlo Malugani <monkodev@gmail.com>
2017-05-22 16:14:37 -0300

    Agrega views new y funcionalidad index

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,4 +3,19 @@ class QuestionsController < ApplicationController
 	def index
 		@question=Question.order(created_at: :desc)
 	end
+	def new
+		@question = current_user.question.build
+	end
+	def create
+		@question = current_user.question.build(question_params)
+		if @question.save
+			redirect_to question_path, notice: "La pregunta se ingreso correctamente"
+		else
+			render :new
+		end
+	end
+	protected
+	def question_params
+      params.require(:question).permit(:titulo, :descripcion)
+end
 end
