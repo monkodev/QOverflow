226e2fbf745ae9b53ea4af3fc8616679751a254f
Giancarlo Malugani <monkodev@gmail.com>
2017-05-23 23:18:44 -0300

    Implementa show. Agrega logos y mejora apariencia

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,12 +4,15 @@ class QuestionsController < ApplicationController
 		@question=Question.order(created_at: :desc)
 	end
 	def new
-		@question = current_user.question.build
+		@question = current_user.questions.build
+	end
+	def show
+		@question = Question.find(params[:id])
 	end
 	def create
-		@question = current_user.question.build(question_params)
+		@question = current_user.questions.build(question_params)
 		if @question.save
-			redirect_to question_path, notice: "La pregunta se ingreso correctamente"
+			redirect_to questions_path, notice: "La pregunta se ingreso correctamente"
 		else
 			render :new
 		end
