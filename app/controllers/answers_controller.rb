4f444c5de762bd3f24e00526df80df5d8802a40c
Giancarlo Malugani <monkodev@gmail.com>
2017-05-26 17:07:24 -0300

    Modifica login devise

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,13 @@
 class AnswersController < ApplicationController
+	def create
+		question = Question.find(params[:question_id])
+  		question.answer.create(answer_params)
+
+  		redirect_to question
+	end
+	
+	protected
+	def answer_params
+      params.require(:answer).permit(:descripcion).merge(user: current_user)
+	end
 end
