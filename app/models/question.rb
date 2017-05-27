2e10c34a7a2a302e131affcba04562e123aab8c7
Giancarlo Malugani <monkodev@gmail.com>
2017-05-27 19:50:53 -0300

    Implementa busqueda de preguntas/contenido

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,8 @@ class Question < ApplicationRecord
 	belongs_to :user
 	has_many :answer
 	has_many :comequestion
+
+	def self.search(search)
+		where("titulo ILIKE ? OR descripcion ILIKE ?","%#{search}%","%#{search}%")
+	end
 end
